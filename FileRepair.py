cccc0f09cba7382ce6512a8313887c706bdeb9b1
psakievich <psakievich@gmail.com>
2016-11-17 15:43:55 -0700

    Add FileRepair

---
+++ FileRepair.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 17 15:26:42 2016
+
+@author: psakievich
+"""
+def ReadBrokenFile(fName,catEveryXLines):
+    dataSet=[]
+    with open(fName) as f:
+        i=0
+        temp=''
+        for line in f:
+            line=line.strip()
+            temp=temp+line
+            i=i+1
+            if(i%catEveryXLines==0):
+                dataSet.append(temp)
+                temp=''
+    return dataSet
