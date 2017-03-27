ab4c7370f6163fc99227db7ba1698abd86afb037
psakievich <psakievich@gmail.com>
2017-03-27 15:28:38 -0700

    Change visit call to onetime save

--- VisitVis/ClientSave.py
+++ VisitVis/ClientSave.py
@@ -1,7 +1,6 @@
 import sys,os,time
-sys.path.append(os.environ["HOME"]+"/Documents/workspace/ASUPython/VisitVis/")
+sys.path.append(os.environ["HOME"]+"/Research/ASUPython/VisitVis/")
 import DocumentPOD as DP
-print "\n",sys.argv
 fName=sys.argv[1]
 AddWindow()
 if fName is not None:
